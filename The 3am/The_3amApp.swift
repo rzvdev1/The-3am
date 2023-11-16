09f9e2b24ff6df8594ec2bda864c37fb29fb115a
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-11-15 22:17:00 -0700

    Initial Commit

---
+++ The 3am/The_3amApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_3amApp.swift
+//  The 3am
+//
+//  Created by Julio Ramirez on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct The_3amApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
