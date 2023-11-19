aa6137bad2415cefe90e8781210eb00f44bddbe9
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-11-18 23:08:57 -0700

    Added start and finish button, added timer to keep track of how long you run, and added a distance you track how far you ran.

--- The 3am/ContentView.swift
+++ The 3am/ContentView.swift
@@ -8,17 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isRunning = false
+    @State private var timer: Timer? = nil
+    @State private var secondsElapsed = 0
+    @State private var distanceRun = 0.0 // Distance run in miles
+
+    let speed = 6.0 // Speed in miles per hour
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, World!")
+            Text("LETS GO!")
+            Text("Seconds: \(secondsElapsed)")
+            Text("Distance: \(distanceRun, specifier: "%.2f") miles") // Display distance run
+            
+            HStack {
+                Button(action: {
+                    startRun()
+                }) {
+                    Text("Start Run")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(8)
+                }
+
+                Button(action: {
+                    finishRun()
+                }) {
+                    Text("Finish Run")
+                        .padding()
+                        .background(Color.red)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(8)
+                }
+            }
         }
         .padding()
     }
+    
+    func startRun() {
+        print("Run started")
+        isRunning = true
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.secondsElapsed += 1
+            self.distanceRun += self.speed / 3600 // Calculate distance run
+        }
+    }
+
+    func finishRun() {
+        print("Run finished")
+        isRunning = false
+        self.timer?.invalidate()
+        self.timer = nil
+    }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
