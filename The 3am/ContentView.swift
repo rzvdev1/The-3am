000b6317545ce54469183bd88783ca45e27966cd
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-11-19 21:17:08 -0700

    dark theme and image added

--- The 3am/ContentView.swift
+++ The 3am/ContentView.swift
@@ -5,24 +5,101 @@
 //  Created by Julio Ramirez on 11/15/23.
 //
 
+//import SwiftUI
+//
+//struct ContentView: View {
+//    @State private var isRunning = false
+//    @State private var timer: Timer? = nil
+//    @State private var secondsElapsed = 0
+//    @State private var distanceRun = 0.0
+//
+//    let speed = 6.0 
+//
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("LETS GO!")
+//            Text("Seconds: \(secondsElapsed)")
+//            Text("Distance: \(distanceRun, specifier: "%.2f") miles")
+//            
+//            HStack {
+//                Button(action: {
+//                    startRun()
+//                }) {
+//                    Text("Start Run")
+//                        .padding()
+//                        .background(Color.blue)
+//                        .foregroundColor(Color.white)
+//                        .cornerRadius(8)
+//                }
+//
+//                Button(action: {
+//                    finishRun()
+//                }) {
+//                    Text("Finish Run")
+//                        .padding()
+//                        .background(Color.red)
+//                        .foregroundColor(Color.white)
+//                        .cornerRadius(8)
+//                }
+//            }
+//        }
+//        .padding()
+//        .preferredColorScheme(.dark)
+//    }
+//    
+//    func startRun() {
+//        print("Run started")
+//        isRunning = true
+//        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+//            self.secondsElapsed += 1
+//            self.distanceRun += self.speed / 3600
+//        }
+//    }
+//
+//    func finishRun() {
+//        print("Run finished")
+//        isRunning = false
+//        self.timer?.invalidate()
+//        self.timer = nil
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
+
 import SwiftUI
+import URLImage
 
 struct ContentView: View {
     @State private var isRunning = false
     @State private var timer: Timer? = nil
     @State private var secondsElapsed = 0
-    @State private var distanceRun = 0.0 // Distance run in miles
+    @State private var distanceRun = 0.0
 
-    let speed = 6.0 // Speed in miles per hour
+    let speed = 6.0
+    //use Image Address
+    let url: URL = URL(string: "https://images.unsplash.com/photo-1544764200-d834fd210a23?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")!
 
     var body: some View {
         VStack {
+            URLImage(url) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("LETS GO!")
             Text("Seconds: \(secondsElapsed)")
-            Text("Distance: \(distanceRun, specifier: "%.2f") miles") // Display distance run
+            Text("Distance: \(distanceRun, specifier: "%.2f") miles")
             
             HStack {
                 Button(action: {
@@ -47,6 +124,7 @@ struct ContentView: View {
             }
         }
         .padding()
+        .preferredColorScheme(.dark)
     }
     
     func startRun() {
@@ -54,7 +132,7 @@ struct ContentView: View {
         isRunning = true
         self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
             self.secondsElapsed += 1
-            self.distanceRun += self.speed / 3600 // Calculate distance run
+            self.distanceRun += self.speed / 3600
         }
     }
 
@@ -71,3 +149,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
